8bdd11661ffea36b0f7d08c25154264133b5f8a7
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2022-04-21 22:29:34 -0400

    Added static capture methods to Optional<CLIWrapper.STDOutputCapturing> to allow easy instantiation of the CLIWrapper.STDOutputCapturing objects where the parameter is optional

---
+++ Sources/CLIWrapper/extensions/Optional+CLIWrapper.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tyler Anger on 2022-04-20.
+//
+
+import Foundation
+import CLICapture
+
+public extension Optional where Wrapped == CLIWrapper.STDOutputCapturing {
+    /// Capture both process buffer and help buffer to the same buffer
+    static func capture(using buffer: CLICapture.STDOutputBuffer) -> CLIWrapper.STDOutputCapturing {
+        return CLIWrapper.STDOutputCapturing.capture(using: buffer)
+    }
+    /// Capture process buffer and help buffer seperatley
+    static func capture(processBuffer: CLICapture.STDOutputBuffer,
+                        helpProcessBuffer: CLICapture.STDOutputBuffer) -> CLIWrapper.STDOutputCapturing {
+        return CLIWrapper.STDOutputCapturing.capture(processBuffer: processBuffer,
+                                                     helpProcessBuffer: helpProcessBuffer)
+    }
+    /// Capture process buffer only
+    static func capture(processBuffer: CLICapture.STDOutputBuffer) -> CLIWrapper.STDOutputCapturing {
+        return CLIWrapper.STDOutputCapturing.capture(processBuffer: processBuffer)
+    }
+    /// Capture help buffer only
+    static func capture(helpProcessBuffer: CLICapture.STDOutputBuffer) -> CLIWrapper.STDOutputCapturing {
+        return CLIWrapper.STDOutputCapturing.capture(helpProcessBuffer: helpProcessBuffer)
+    }
+}
